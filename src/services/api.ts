2dc213c55cd31c43c0c48c2179ec2c2f7c7d193a
JwRicauter <jwricauter@gmail.com>
2023-03-31 15:05:07 +0200

    new detail podcast endpoints added

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,13 +2,42 @@ export const getPodcasts = async (signal: AbortSignal)  => {
 
   const uri : string = process.env.REACT_APP_ITUNES_URL + 'us/rss/toppodcasts/limit=100/genre=1310/json';
   let response = await fetch(uri, {
-      method: 'GET',
-      signal: signal
-}).then(
-      res => res.json()
+    method: 'GET',
+    signal: signal
+  }).then(
+    res => res.json()
   ).catch(
-  err => {return err}
-)
+    err => {return err}
+  )
+  
   return response;
+}
 
+export const getDetail = async (signal: AbortSignal, podcastId: string) => {
+    
+  const uri : string = 'https://api.allorigins.win/get?url=' + process.env.REACT_APP_ITUNES_URL + `lookup?id=${podcastId}`;
+
+  let response = await fetch(uri, {
+    method: 'GET',
+    signal: signal
+  }).then(
+    res => res.json()
+  ).catch(
+    err => {return err}
+  )
+  
+  return response;
+}
+
+export const getEpisodes = async (uri: string) => {
+  
+  let response = await fetch(uri, {
+    method: 'GET',
+  }).then(
+    res => res.text()
+  ).catch(
+    err => {return err}
+  )
+  
+  return response;
 }
