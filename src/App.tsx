e3b49e66c165ad893ab8b8313022b20f32f0e13d
JwRicauter <jwricauter@gmail.com>
2023-03-31 13:25:19 +0200

    creating podcast detail page

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import { Routes, Route } from 'react-router-dom';
 
 import { Home } from './pages/Home';
 import { Layout } from './Layout';
-
+import { PodcastDetail } from './pages/PodcastDetail';
 
 import './App.scss';
 
@@ -13,6 +13,7 @@ function App() {
     <Routes>
       <Route path="/" element={<Layout />}>
         <Route path="/" element={<Home />} />
+        <Route path="/podcast/:id" element={<PodcastDetail />} />
       </Route>
     </Routes>
   );
